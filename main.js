f47bea4bcb007cabae757567063cf1528449c501
IMAtendra <66991043+IMAtendra@users.noreply.github.com>
2023-12-29 12:03:01 +0530

    Initial Files
    Files are
    - index.html
    - style.css
    - main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// Input value change the box properties
+
+let box = document.querySelector(".box");
+let input = document.querySelector("input");
+
+input.addEventListener("input", () => {
+    box.style.background = input.value;
+    box.style.borderRadius = input.value;
+});
